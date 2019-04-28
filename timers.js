d7e03e176a846f243dc6fd48b273df05f2e7863c
utamaru <anything@utamaru.com>
2019-04-28 17:19:40 +0200

    okay

--- timers.js
+++ timers.js
@@ -1,1 +1,8 @@
-alert('hello');
+var myVar = setInterval(function() {
+	myTimer();
+}, 1000);
+
+function myTimer() {
+	var d = new Date();
+	document.getElementById("text1").innerHTML = d.toLocaleTimeString();
+}
