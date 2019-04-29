282e012512bff5e3e5f570e21b4568e7ed3a066e
utamaru <anything@utamaru.com>
2019-04-29 19:13:22 +0200

    cleanup and comments

--- main.js
+++ main.js
@@ -55,86 +55,86 @@ function alarm(text,sound) {
 }
 
 /* Milliseconds Checkbox */
-function msVisCheck() {
-	var checkBox = document.getElementById("msCheck");
-	var text = document.getElementById("text4");
-	var div = document.getElementById("timediv");
+	function msVisCheck() {
+		var checkBox = document.getElementById("msCheck");
+		var text = document.getElementById("text4");
+		var div = document.getElementById("timediv");
 
-	var delay = 200;
+		var delay = 200;
 
-	if (checkBox.checked == true) {	
-		text.style.visibility = "visible";
-		msVisible = true;
-		text.style.color = "rgba(255,255,255,1)";
-		div.style.width = "160px";
+		if (checkBox.checked == true) {	
+			text.style.visibility = "visible";
+			msVisible = true;
+			text.style.color = "rgba(255,255,255,1)";
+			div.style.width = "160px";
 
-	}
-	else { 
-		msVisible = false;
-		text.style.color = "rgba(255,255,255,0)";
-		div.style.width = "130px";
-		setTimeout(function(){
-			text.style.visibility = "hidden";
-		}, delay);
-		
+		}
+		else { 
+			msVisible = false;
+			text.style.color = "rgba(255,255,255,0)";
+			div.style.width = "130px";
+			setTimeout(function(){
+				text.style.visibility = "hidden";
+			}, delay);
+			
 
-	}
-}
-
-/* Timestamp Checkbox */
-function tsVisCheck() {
-	var checkBox = document.getElementById("tsCheck");
-	if (checkBox.checked == true) {	
-		timeStamps = true;
-	}
-	else { 
-		timeStamps = false;
-	}
-}
-
-function addZero(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
-}
-
-function addZeroMs(i) {
-  if (i > 999){
-  	i = 999;
-  }
-  else if (i < 10) {
-    i = "00" + i;
-  }
-  else if (i < 100) {
-  	i = "0" + i;
-  }
-  return i;
-}
-
-function myTimer() {
-	var d = new Date();
-	if (timerRunning){
-		document.getElementById("timediv").style.background = "green";
-	}
-	else {
-		document.getElementById("timediv").style.background = "black";
+		}
 	}
 
-	hours = addZero(d.getHours());
-	minutes = addZero(d.getMinutes());
-	seconds = addZero(d.getSeconds());
-	ms = addZeroMs(d.getMilliseconds());
-
-	document.getElementById("text1").innerHTML = "" + hours;
-	document.getElementById("text2").innerHTML = ":" + minutes;
-	document.getElementById("text3").innerHTML = ":" + seconds;
-
-	if(msVisible == true){
-		document.getElementById("text4").innerHTML = "." + ms;
+	/* Timestamp Checkbox */
+	function tsVisCheck() {
+		var checkBox = document.getElementById("tsCheck");
+		if (checkBox.checked == true) {	
+			timeStamps = true;
+		}
+		else { 
+			timeStamps = false;
+		}
 	}
-}
 
+	function addZero(i) {
+	  if (i < 10) {
+	    i = "0" + i;
+	  }
+	  return i;
+	}
+
+	function addZeroMs(i) {
+	  if (i > 999){
+	  	i = 999;
+	  }
+	  else if (i < 10) {
+	    i = "00" + i;
+	  }
+	  else if (i < 100) {
+	  	i = "0" + i;
+	  }
+	  return i;
+	}
+/* Main Function */
+	function myTimer() {
+		var d = new Date();
+		if (timerRunning){
+			document.getElementById("timediv").style.background = "green";
+		}
+		else {
+			document.getElementById("timediv").style.background = "black";
+		}
+
+		hours = addZero(d.getHours());
+		minutes = addZero(d.getMinutes());
+		seconds = addZero(d.getSeconds());
+		ms = addZeroMs(d.getMilliseconds());
+
+		document.getElementById("text1").innerHTML = "" + hours;
+		document.getElementById("text2").innerHTML = ":" + minutes;
+		document.getElementById("text3").innerHTML = ":" + seconds;
+
+		if(msVisible == true){
+			document.getElementById("text4").innerHTML = "." + ms;
+		}
+	}
+/* Buttons */
 function button() {
 	appendText("button pressed");
 
@@ -144,6 +144,9 @@ function button() {
 		console.log("time elapsed");
 		alarm("boing","alarm.mp3"); 
 	}, 2000)
+}
+function button2() {
+	appendText("button2 pressed");
 }
 
 function menuShow(i){
