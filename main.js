0344f3a7071a7e7621c9df49ac4eb4015d67464c
utamaru <anything@utamaru.com>
2019-05-01 15:07:58 +0200

    custom dialog for timers

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // loops Main function at 11ms rate
-var myVar = setInterval(myTimer, 11);
+var myVar = setInterval(mainLoop, 11);
 
 //defaults
 var msVisible = true;
@@ -23,7 +23,7 @@ var timerOrigin;
 	;
 	*/
 /* Main Function */
-	function myTimer() {
+	function mainLoop() {
 		var d = new Date();
 		if (timerRunning){
 			document.getElementById("timediv").style.background = "green";
@@ -47,20 +47,55 @@ var timerOrigin;
 
 		renderBars();
 	}
+	function renderTime(i) {
+		var d = new Date(i);
+
+		H = d.getHours() - 1;
+		M = d.getMinutes();
+		S = d.getSeconds();
+
+		h = (H>0) ? addZero(H) + ":" : "";
+		m = (M>0) ? addZero(M) + ":" : "";
+		s = addZero(S);
+
+		return h+m+s
+	}
+	function renderMs(i) {
+		var d = new Date(i);
+		return "." + addZeroMs(d.getMilliseconds())
+	}
 
 /** EGGTIMER **/
-	function eggTimer(eS,eM,eH,style) {
+	function newTimer(){
+		form = document.getElementById("newTimer");
+		appendText(form.countdown.checked);
 
+		h = form.h.value;
+		m = form.m.value;
+		s = form.s.value;
+		size = form.size.value;
+		text = form.text.value;
+
+		if (form.countdown.checked){ style = "countdown " + size; }
+		else { style = size }
+
+		eggTimer(s,m,h,style,text);
+
+		form.reset();
+	}
+
+	function eggTimer(eS,eM,eH,style,text) {
 		if (eH == undefined) { eH = 0; }
 		if (eM == undefined) { eM = 0; }
+		if (text == undefined) { text = "eggtimer (undefined)" }
 	   var cookingTime = ( eS * 1000 ) + ( eM * 60 * 1000) + ( eH * 60 * 60 * 1000 );
 
-		appendText("egg timer starting, " + cookingTime + " milliseconds");
+		appendText(text + " starting, " + cookingTime + " milliseconds");
 
 
 		if (!style) { style = "normal"; }
 
-		startTimer(cookingTime,"egg",0,style);
+		startTimer(cookingTime,text,0,style);
 		menuHide();
 	}
 
@@ -68,8 +103,8 @@ var timerOrigin;
 
 	function startTimer(T,text,soundID,style) {
 		var d = new Date(); 
-		
 		var f = new Date(d.valueOf() + T);
+
 		if (!style) { style = "normal"; }
 
 		activeTimers.push({
@@ -122,7 +157,11 @@ var timerOrigin;
 		var parent = document.getElementById('bars');
 		var div = document.createElement('div');
 		var bar = document.createElement('div');
+		
 		var label = document.createElement('p');
+		var countdown = document.createElement('p');
+		var msText = document.createElement('span');
+		var timeText = document.createElement('span');
 
 		div.setAttribute('class',"bar" + " " + activeTimers[ID].style);
 		div.setAttribute('id',"bar" + ID);
@@ -130,9 +169,22 @@ var timerOrigin;
 		bar.setAttribute('id',"progress" + ID);
 
 		label.setAttribute('id',"barlabel" + ID);
+		label.setAttribute('class',"barlabel");
    	label.innerHTML = activeTimers[ID].text;
 
+   	// countdown.setAttribute('id',"barcountdown"+ ID);
+   	countdown.setAttribute('class',"barcountdown");
+
+   	timeText.setAttribute('class',"barcountdown")
+   	timeText.setAttribute('id',"barcountdown"+ ID);
+   	msText.setAttribute('class',"barcountdownms");
+   	msText.setAttribute('id',"barcountdownms" + ID);
+
+   	countdown.appendChild(timeText);
+   	countdown.appendChild(msText);
+
 		div.appendChild(label);
+		div.appendChild(countdown);
 		div.appendChild(bar);
 
 		parent.appendChild(div);
@@ -142,10 +194,12 @@ var timerOrigin;
 		var div = document.getElementById('bar'+ID);
 		var bar = document.getElementById('progress' + ID)
 		var label = document.getElementById('barlabel' + ID);
+		var countdown = document.getElementById('barcountdown' + ID);
 
 
 
 		label.style.color = "rgba(0,0,0,0)";
+		countdown.style.color = "rgba(0,0,0,0)";
 		bar.style.height = "0px";
 		div.style.height = "0px";
 		div.style.marginBottom = "0px";
@@ -160,30 +214,33 @@ var timerOrigin;
 
 		for (i = 0; i < num; i++){
 			current = activeTimers[i];
-			bar = document.getElementById("progress" + activeTimers[i].ID);
+			if(current.active){
+				bar = document.getElementById("progress" + activeTimers[i].ID);
 
-			start = activeTimers[i].start.valueOf();
-			end = activeTimers[i].finish.valueOf();
+				start = current.start.valueOf();
+				end = current.finish.valueOf();
+				elapsed = now-start;
+				total = end-start;
+				remainder = total-elapsed;
 
-			elapsed = now-start;
-			total = end-start;
+				elapsedFraction = (elapsed/total*100);
 
-			elapsedFraction = (elapsed/total*100);
+				if (elapsedFraction > 100) 	{ elapsedFraction = 100 }
 
-			if (elapsedFraction > 100) 	{ elapsedFraction = 100 }
+				if (current.inverted) { widthPercent = 100-elapsedFraction }
+				else 									{ widthPercent = elapsedFraction }
 
-			if (activeTimers[i].inverted) { widthPercent = 100-elapsedFraction }
-			else 									{ widthPercent = elapsedFraction }
+				if (current.countdown) {
+					countdown = document.getElementById("barcountdown" + current.ID);
+					ms = 			document.getElementById("barcountdownms" + current.ID);
+					countdown.innerHTML = renderTime(remainder);
+					ms.innerHTML		  = renderMs(remainder);
+				}
+				widthPercent = widthPercent + "%";
 
-			if (current.countdown){
-				label = document.getElementById("barlabel" + current.ID);
-				label.innerHTML = elapsed;
-			}
-			widthPercent = widthPercent + "%";
-
-			if (bar != undefined){ 
 				bar.style.width = widthPercent;
 				if (elapsedFraction > 80) bar.style.background = "red";
+				
 			}
 		}
 	}
@@ -192,14 +249,16 @@ var timerOrigin;
 /* Buttons and menu */
 	function button() {
 		appendText("button pressed","status");
-		eggTimer(3);
+		eggTimer(20,4,0,"big countdown","bigtest");
+		eggTimer(2,3,0,"countdown","medium test");
+		eggTimer(30,0,0,"small","smalltest");
 	}
 	function button2() {
 		appendText("button2 pressed");
 	}
 
 	function menuShow(i){
-		var bg = document.getElementById("menuwrapper");
+		var bg = document.getElementById("menuBG");
 		var focus = document.getElementById(i);
 
 		windowState = i;
@@ -211,7 +270,7 @@ var timerOrigin;
 
 	function menuHide(){
 			if (windowState){
-			var bg = document.getElementById("menuwrapper");
+			var bg = document.getElementById("menuBG");
 			var focus = document.getElementById(windowState);
 
 			windowState = false;
@@ -235,21 +294,23 @@ var timerOrigin;
 			var delay = 200;
 
 			if (checkBox.checked == true) {	
-				text.style.visibility = "visible";
 				msVisible = true;
+
 				text.style.color = "rgba(255,255,255,1)";
+				text.style.visibility = "visible";
+
 				div.style.width = "160px";
 
 			}
 			else { 
 				msVisible = false;
+
 				text.style.color = "rgba(255,255,255,0)";
-				div.style.width = "130px";
 				setTimeout(function(){
 					text.style.visibility = "hidden";
 				}, delay);
 				
-
+				div.style.width = "130px";
 			}
 	}
 
