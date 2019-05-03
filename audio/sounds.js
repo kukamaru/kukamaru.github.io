f1905000d23cfe812d965ce56b5c54b6d196f687
utamaru <anything@utamaru.com>
2019-05-03 13:31:49 +0200

    Split out sounds, text stuff. Working.

---
+++ audio/sounds.js
@@ -0,0 +1,25 @@
+const sounds = [
+
+
+{ 
+	soundName: "eggsound",
+	src: "audio/eggsound1.mp3",
+	looping: false
+},
+
+
+{
+	soundName: "jingle",
+	src: "audio/jingle.mp3",
+	looping: false
+},
+
+
+{
+	soundName: "groove",
+	src: "audio/groove.mp3",
+	looping: true
+}
+
+
+];
---
+++ debugtext.js
@@ -0,0 +1,40 @@
+iT = 0;
+function appendText(i,c) {
+	var text = i;
+	var textClass = evenOdd(iT);
+	if (c){	textClass = textClass + " " + c; }
+
+	var node = document.createElement("p");
+	var textnode = document.createTextNode(text);
+
+	node.setAttribute("id",iT);
+	node.setAttribute("class",textClass);
+
+	if (timeStamps){
+		var d = new Date();
+
+		var hours = addZero(d.getHours());
+		var minutes = addZero(d.getMinutes());
+	   var seconds = addZero(d.getSeconds());
+
+		var ts = "[" + hours + ":" + minutes + ":" + seconds + "] ";
+
+		var tsText = document.createTextNode(ts);
+		var tsNode = document.createElement("span");
+		tsNode.appendChild(tsText);
+		tsNode.setAttribute("class","timestamp");
+
+		node.appendChild(tsNode)
+	}
+	node.appendChild(textnode);
+
+	document.getElementById("maintext").appendChild(node);
+
+	iT = iT + 1;
+}
+
+function clearText() {
+	while(maintext.firstChild){
+		maintext.removeChild(maintext.firstChild);
+	}
+}
